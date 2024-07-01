5ff5112b327568a63b7b78a9da9a051ed1429073
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-07-01 19:06:50 +0100

    Update FlutterAutofillService.kt

--- android/src/main/kotlin/com/keevault/flutter_autofill_service/FlutterAutofillService.kt
+++ android/src/main/kotlin/com/keevault/flutter_autofill_service/FlutterAutofillService.kt
@@ -193,7 +193,7 @@ class FlutterAutofillService : AutofillService() {
                 this,
                 1230,
                 startAuthIntent,
-                PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_ALLOW_UNSAFE_IMPLICIT_INTENT
+                PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_MUTABLE
             )
         } else {
             @SuppressLint("UnspecifiedImmutableFlag")
@@ -201,7 +201,7 @@ class FlutterAutofillService : AutofillService() {
                 this,
                 1230,
                 startAuthIntent,
-                PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_ALLOW_UNSAFE_IMPLICIT_INTENT
+                PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_MUTABLE
             )
         }
         val intentSender: IntentSender = pendingIntent.intentSender
